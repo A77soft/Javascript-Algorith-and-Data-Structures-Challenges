f8dfcc712057f759060cb0c3ba531ee43ef78f01
A77soft <62488561+A77soft@users.noreply.github.com>
2021-02-01 09:01:30 -0500

    Create returnIndexForInsertion.js
    An exercise in creating an algorithm that returns the earliest possible index for inserting a passed number into an array.

---
+++ returnIndexForInsertion.js
@@ -0,0 +1,9 @@
+function returnIndexForInsertion(arr, num) {
+    arr.sort((a, b) => a - b);
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] >= num) {
+        return i;
+      }
+    }
+    return arr.length;
+  }
