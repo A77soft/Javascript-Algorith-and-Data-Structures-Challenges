fdae091140c3c00f65f3c959ba97de422fc24b77
A77soft <62488561+A77soft@users.noreply.github.com>
2021-02-02 09:08:31 -0500

    Create compareStringCharacters.js
    An exercise in creating an algorithm that returns true if the first element of the array contains all of the letters of the string in the second element of the array.

---
+++ compareStringCharacters.js
@@ -0,0 +1,10 @@
+function compareStringCharacters(arr) {
+    let target = arr[0].toLowerCase();
+    let test = arr[1].toLowerCase();
+    for (let i = 0; i < test.length; i++) {
+      if (target.indexOf(test[i]) === -1) {
+        return false;
+      }
+    }
+    return true;
+  }
