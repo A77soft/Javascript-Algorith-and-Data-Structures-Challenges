b3f6421f1257c5b7b3ea537c69eab93ea0c70087
A77soft <62488561+A77soft@users.noreply.github.com>
2021-02-10 08:48:45 -0500

    Create caseSensitiveSearchReplace.js
    An exercise in creating an algorithm that performs a search and replace on the sentence using the arguments provided while preserving the case of the first character of the original word.

---
+++ caseSensitiveSearchReplace.js
@@ -0,0 +1,9 @@
+function searchReplace(str, before, after) {
+    let index = str.indexOf(before);
+    if (str[index] === str[index].toUpperCase()) {
+      after = after[0].toUpperCase() + after.slice(1);
+    } else {
+      after = after[0].toLowerCase() + after.slice(1);
+    }
+    return str.replace(before, after);
+  }
