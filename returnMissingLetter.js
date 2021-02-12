c55311db24b323172a7ad92260301eeec4a705bf
A77soft <62488561+A77soft@users.noreply.github.com>
2021-02-12 09:21:52 -0500

    Create returnMissingLetter.js
    An exercise in creating an algorithm that finds the missing letter in the passed string and returns it.

---
+++ returnMissingLetter.js
@@ -0,0 +1,9 @@
+function returnMissingLetter(str) {
+    for (let i = 0; i < str.length; i++) {
+      let code = str.charCodeAt(i);
+      if (code !== str.charCodeAt(0) + i) {
+        return String.fromCharCode(code - 1);
+      }
+    }
+    return undefined;
+  }
