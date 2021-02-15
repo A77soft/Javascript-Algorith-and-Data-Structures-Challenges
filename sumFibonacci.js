8d8dd3b3a4eb91d05ec45eff22e2d1584dc3a9a8
A77soft <62488561+A77soft@users.noreply.github.com>
2021-02-15 10:07:56 -0500

    Create sumFibonacci.js
    An exercise in creating an algorithm that returns the sum of all odd Fibonacci numbers that are less than or equal to passed number.

---
+++ sumFibonacci.js
@@ -0,0 +1,13 @@
+function sumFibonacci(num) {
+    let prevNumber = 0;
+    let currNumber = 1;
+    let result = 0;
+    while (currNumber <= num) {
+      if (currNumber % 2 !== 0) {
+        result += currNumber;
+      }
+      currNumber += prevNumber;
+      prevNumber = currNumber - prevNumber;
+    }
+    return result;
+  }
