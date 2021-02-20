a6d5a6208c26585b26703201f8003e2499256d92
A77soft <62488561+A77soft@users.noreply.github.com>
2021-02-20 08:54:17 -0500

    Create truthCheck.js
    An exercise in creating an algorithm that  checks if the second argument is truthy on all elements of a collection.

---
+++ truthCheck.js
@@ -0,0 +1,21 @@
+//1st Method
+function truthCheckOne(collection, pre) {
+    for (var i = 0; i < collection.length; i++){
+    if (collection[i][(pre)] === false || collection[i][(pre)] === 0 || collection[i][(pre)] === "" || collection[i][(pre)] === null || collection[i][(pre)] === undefined || Number.isNaN(collection[i][(pre)] )=== true){
+    return false;
+    }
+    }
+    return true;
+   }
+
+//2nd Method
+function truthCheck(collection, pre) {
+    let counter = 0;
+    for (let i in collection) {
+      if (collection[i].hasOwnProperty(pre) && Boolean(collection[i][pre])) {
+        counter++;
+      }
+    }
+    return counter == collection.length;
+  }
+  
