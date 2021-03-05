6d46c4dc4c05f8653bf751f426421ad4e7e0f069
A77soft <62488561+A77soft@users.noreply.github.com>
2021-03-05 07:56:53 -0500

    Update dropElements.js
    Appended a new implementation method.

--- dropElements.js
+++ dropElements.js
@@ -1,6 +1,15 @@
+//1st Implementation
 function dropElements(arr, func) {
     while (!func(arr[0])) {
       arr.shift()
     }
     return arr;
+}
+
+//2nd Implementation
+function dropElements(arr, func) {
+ while (func(arr[0])===false){
+  arr.splice(0, 1);
   }
+ return arr;
+}
