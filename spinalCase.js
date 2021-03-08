8fa53cc67ee11423b0d38393028bf09dbbe9fd8c
A77soft <62488561+A77soft@users.noreply.github.com>
2021-03-08 10:12:17 -0500

    Update spinalCase.js
    Appended a new implementation method.

--- spinalCase.js
+++ spinalCase.js
@@ -1,5 +1,12 @@
-function spinalCase(str) {
+function spinalCaseOne(str) {
     let regex = /\s+|_+/g;
     str = str.replace(/([a-z])([A-Z])/g, "$1 $2");
     return str.replace(regex, "-").toLowerCase();
   }
+
+function spinalCaseTwo(str) {
+  return str
+        .replace(/([a-z])([A-Z])/g, "$1 $2")
+        .replace(/\s+|_+/g, "-")
+        .toLowerCase();
+}
